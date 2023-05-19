e786399bdac48fafda53052ff0718aede6a4561a
Yesua <65790207+YesuaPV@users.noreply.github.com>
2023-05-18 22:56:03 -0600

    Interfaz de log in

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,6 +4,7 @@ import HomeContainer from '../containers/homeContainer';
 import ProfileContainer from '../containers/profileContainer';
 import SolicitudesContainer from '../containers/solicitudesContainer';
 import StatisticsContainer from '../containers/statisticsContainer';
+import LoginContainer from '../containers/loginContainer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/profile" element={<ProfileContainer />}/>
           <Route exact path="/solicitudes" element={<SolicitudesContainer />}/>
           <Route exact path="/estadisticas" element={<StatisticsContainer />}/>
+          <Route exact path="/login" element={<LoginContainer />}/>
         </Routes>
       </Router>
   );
